45bb5b554c82c442cc27d3de24247ed4e6b14518
ZUA0001 <yazan.qutshan@icloud.com>
2025-07-04 14:11:26 +0200

    stand 04.07.2025

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
 console.log("ZUA Onepage aktiv 🧠🔥");
-
-const toggle = document.getElementById("darkmode-toggle");
-toggle.addEventListener("click", () => {
-    document.body.classList.toggle("lightmode");
-});
